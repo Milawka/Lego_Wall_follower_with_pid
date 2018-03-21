776b7b6b3f08f807468f9b21ea41f27a9af55c76
Milana <milanochka375@mail.ru>
2018-03-22 00:13:23 +0300

    initial commit

---
+++ lego.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from ev3dev.ev3 import *
+from time import sleep
+
+
+sensor = UltrasonicSensor("in1")
+tsensor = TouchSensor("in2")
+motorLeft = LargeMotor("outA")
+motorRight = LargeMotor("outB")
+
+run = True
+
+desiredLength = 25
+speed = 350
+
+threshold = 20
+error = 0
+lastError = 0
+proportional = 0
+integral = 0
+derivative = 0
+
+pidValue = 0
+
+kP = 10
+kI = 0.01
+kD = 50
+
+motorLeft.run_timed(time_sp=3000, speed_sp=(speed))
+motorRight.run_timed(time_sp=3000, speed_sp=(speed))
+
+while run:
+    sensorValue = sensor.value() / 10
+
+    error = sensorValue - desiredLength
+    if abs(error) < threshold:
+        integral += error
+    else:
+        integral = 0
+    derivative = error - lastError
+    lastError = error
+    pidValue = error*kP + integral*kI + derivative*kD
+
+    if pidValue > 250:
+        pidValue = 250
+    elif pidValue < -1 * 250:
+        pidValue = -1*250
+
+    motorRight.run_timed(time_sp=3000, speed_sp=(speed + pidValue))
+    motorLeft.run_timed(time_sp=3000, speed_sp=(speed - pidValue))
+
+    if ts.value():
+      run = False
+
+motorRight.stop()
+motorLeft.stop()
+
